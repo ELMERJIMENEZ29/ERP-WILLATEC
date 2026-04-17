9248cc0d712b9ee40f15155e753982bec147237f
Elmer Jimenez <eldagiar_29@hotmail.com>
2026-04-17 18:40:01 -0500

    feat: add ProductController and Routes Products in api.php

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\UserController;
+use App\Http\Controllers\Api\ProductoController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use Spatie\Permission\Models\Role;
@@ -11,10 +12,9 @@ Route::post('/login', [AuthController::class, 'login']);
 Route::middleware(['auth:sanctum'])->group(function(){
     Route::get('/user', fn(Request $request)=> $request->user());
 
-    Route::get('/roles', function () {
-        return Role::select('id','name')->get();
-    });
+    Route::get('/roles', function () {return Role::select('id','name')->get();});
 
+    //USUARIOS
     Route::post('/register',[AuthController::class, 'register'])->middleware(['role:superadmin']);
 
     Route::get('/users', [UserController::class, 'index'])->middleware('role:superadmin|admin');
@@ -23,10 +23,22 @@ Route::middleware(['auth:sanctum'])->group(function(){
 
     Route::put('/users/{id}', [UserController::class, 'update'])->middleware('role:superadmin');
 
-    Route::patch('/users/{id}/desactivar', [UserController::class, 'desactivar'])
-    ->middleware('role:superadmin');
+    Route::patch('/users/{id}/desactivar', [UserController::class, 'desactivar'])->middleware('role:superadmin');
 
-    Route::patch('/users/{id}/activar', [UserController::class, 'activar'])
-    ->middleware('role:superadmin');
+    Route::patch('/users/{id}/activar', [UserController::class, 'activar'])->middleware('role:superadmin');
+});
+
+Route::prefix('productos')->middleware('auth:sanctum')->group(function(){
+    //PRODUCTOS
+    Route::get('/', [ProductoController::class, 'index']);
+    Route::get('/{id}', [ProductoController::class, 'show']);
+
+    Route::post('/', [ProductoController::class, 'store'])->middleware('role:superadmin|admin');
+
+    Route::put('/{id}', [ProductoController::class, 'update'])->middleware('role:superadmin|admin');
+
+    Route::patch('/{id}/desactivar', [ProductoController::class, 'desactivar'])->middleware('role:superadmin');
+
+    Route::patch('/{id}/activar', [ProductoController::class, 'activar'])->middleware('role:superadmin');
 });
 
